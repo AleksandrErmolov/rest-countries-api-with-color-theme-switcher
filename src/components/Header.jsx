c54e2efd9200a1403276069a410abd0ba0c57bfa
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-01-10 12:36:01 +0300

    add style  header and change theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,75 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import styled from 'styled-components'
 import { IoMoonOutline, IoMoon } from "react-icons/io5";
 
 import { Container } from './Container'
 
+const HeaderEl = styled.header`
+    box-shadow: var(--shadow);
+    background-color: var(--colors-ui-base);
+    `;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    @media (min-width: 767px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }`;
+
+const Title = styled.a.attrs({
+    href: '/',
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
+
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  // font-weight: var(--fw-bold);
+  text-transform: capitalize;
+`;
+
+
+
+
+
 export default function Header() {
 
-    const HeaderEl = styled.header``;
+    const [theme, setTheme] = useState('light')
 
-    const Wrapper = styled.div``;
 
-    const Title = styled.a.attrs({
-        href: '/'
-    })``;
+    const toggleTheme = (e) => {
+        e.preventDefault();
+        setTheme(theme === 'light' ? 'dark' : 'light');
+        console.log(theme)
+    }
 
-    const ModeSwitcher = styled.div``;
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
 
 
     return (
         <HeaderEl>
             <Container>
                 <Wrapper>
-                    <Title>
-                        Where is the world?
-                        <ModeSwitcher>
-                            <IoMoon /> Light Theme
+                    <Title> Where is the world? </Title>
+                    <ModeSwitcher onClick={toggleTheme}>
+                        {theme === 'ligth'
+                            ? (<IoMoonOutline size="14px" />)
+                            : (<IoMoon size="14px" />)
+                        }
+                        <span style={{ marginLeft: '0.75rem' }}> {theme} Theme</span>
                         </ModeSwitcher>
-                    </Title>
+                   
                 </Wrapper>
             </Container>
         </HeaderEl>
